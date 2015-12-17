560833c12b4fd0119c6cf6fbf53747f2ca2637ff
rifqifatih <rifqialfatih@gmail.com>
2015-12-17 16:14:37 +0700

    Able to take photo.

--- app/src/main/java/edu/ubi/selanjutnya/sharepic/MainActivity.java
+++ app/src/main/java/edu/ubi/selanjutnya/sharepic/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.ubi.selanjutnya.sharepic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,5 +49,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void takePhoto(View view) {
+        Intent intent = new Intent(this, CameraActivity.class);
+        // TODO password & name
+        startActivity(intent);
+
     }
 }
